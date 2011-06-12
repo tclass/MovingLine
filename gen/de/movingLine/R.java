f329a3b0cad5b698dcfb974875f139805660237b
Royalclass <Tobi@.(none)>
2011-06-12 20:08:41 +0200

    modified:   bin/MovingLine.apk 	modified:   gen/de/movingLine/R.java 	modified:   res/layout/main.xml 	modified:   src/de/movingLine/MovingLine.java 	bin/de/movingLine/game/ 	res/drawable/ 	src/de/movingLine/game/

--- gen/de/movingLine/R.java
+++ gen/de/movingLine/R.java
@@ -11,13 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int green=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int red=0x7f020002;
+        public static final int yellow=0x7f020003;
     }
     public static final class id {
-        public static final int btnHighscore=0x7f050002;
-        public static final int btnInformation=0x7f050003;
-        public static final int btnNewGame=0x7f050001;
-        public static final int textView1=0x7f050000;
+        public static final int gameView=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
