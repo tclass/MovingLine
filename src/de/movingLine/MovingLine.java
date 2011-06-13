8689e107e2592a18ac869ec9e14a1bda652e8871
Royalclass <Tobi@Royalclass-PC.fritz.box>
2011-06-13 12:32:48 +0200

    changed MovingLine from MenuView to GameHolder, so we can see first develop the game and then the Menu

--- src/de/movingLine/MovingLine.java
+++ src/de/movingLine/MovingLine.java
@@ -2,30 +2,35 @@ package de.movingLine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import de.movingLine.game.GameView;
+import de.movingLine.game.MyGestureDetector;
 
 public class MovingLine extends Activity {
-	
-	
-	GameView gameView;
 
+	private GameView gameView;
+	private GestureDetector gestureDetector;
 
-	
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        gameView = (GameView) findViewById(R.id.gameView);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-        
-        
-    }
-    
+		gameView = (GameView) findViewById(R.id.gameView);
 
-
+		gestureDetector = new GestureDetector(new MyGestureDetector(gameView));
+		gameView.setOnTouchListener(new View.OnTouchListener() {
+	            public boolean onTouch(View v, MotionEvent event) {
+	                if (gestureDetector.onTouchEvent(event)) {
+	                    return true;
+	                }
+	                return false;
+	            }
+	        });	
+	}
 
 
 }
